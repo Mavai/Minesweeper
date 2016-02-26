bba457795e133eae7aad7fc56fcb2fbfbdecc473
Mavai <marko.vainio14@gmail.com>
2016-02-26 12:37:30 +0200

    projektia päivitetty

--- miinaharava/src/main/java/miinaharava/Main.java
+++ miinaharava/src/main/java/miinaharava/Main.java
@@ -11,18 +11,8 @@ import miinaharava.logiikka.Vaikeusaste;
 public class Main {
     
     public static void main(String[] args) {
-        Pelialusta alusta = new Pelialusta(10, 10, 1);
-        Miinaharava peli = new Miinaharava(alusta);
-        //Tekstikayttoliittyma tkl = new Tekstikayttoliittyma(alusta, new Scanner(System.in), new Miinaharava(Vaikeusaste.HELPPO));
-        //tkl.aloita();
         AloitusGui kl = new AloitusGui();
         SwingUtilities.invokeLater(kl);
-        
-//        Scanner lukija = new Scanner(System.in);
-//        
-//        Miinaharava peli = new Miinaharava(alusta, lukija);
-//        
-//        peli.aloita();
         
     }
     
